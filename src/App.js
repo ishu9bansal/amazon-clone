d74e3da4fc223465ff5ce60eac849186f13eeb84
Ishu Bansal <ishubansal1400@gmail.com>
2025-01-14 18:35:41 +0530

    add simple layout and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
 
+const categories = ["All", "Fresh", "MX Player", "Sell", "Lists", "Amazon Pay"];
+
 function App() {
   return (
     <div className="App">
-      Amazon
+      <header className='header'>
+        <div className='navbar'>
+          <div className='navbar-left'>
+            <div className='logo'>Logo</div>
+            <div className='location'>Location</div>
+          </div>
+          <div className='navbar-middle'>
+            <div className='search'>Search Bar</div>
+          </div>
+          <div className='navbar-right'>
+            <div className='language'>Language</div>
+            <div className='account'>Account</div>
+            <div className='orders'>Orders</div>
+            <div className='cart'>Cart</div>
+          </div>
+        </div>
+        <div className='category-bar'>
+          {categories.map(category => <div key={category}>{category}</div>)}
+        </div>
+      </header>
+      <div>Content</div>
+      <footer className='footer'>Footer</footer>
     </div>
   );
 }
