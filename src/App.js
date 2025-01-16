0cc1f7f22c0498ccffb7078e4ccde915ba650bc8
Ishu Bansal <ishubansal1400@gmail.com>
2025-01-16 09:35:48 +0530

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
 import './App.css';
 import bgImg from './assets/carousalImg8.jpg';
-import proImg from './assets/amazonproduct1.jpg'
+import proImg from './assets/amazonproduct1.jpg';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 const categories = ["All", "Fresh", "MX Player", "Sell", "Lists", "Amazon Pay"];
 function App() {
   return (
-    <div className="App">
-      <header className='header'>
-        <Navbar />
-        <div className='category-bar'>
-          {categories.map(category => <div key={category}>{category}</div>)}
-        </div>
-      </header>
-      <div>
-        <div className='bg-image'>
-          <img src={bgImg} style={{ width: '100%' }}></img>
-        </div>
-        <div className='content'>
-          <div>Deals Section</div>
-          <div>Carousal Section</div>
-          <ProductSection />
-        </div>
+    <Router>
+      <div className="App">
+        <header className='header'>
+          <Navbar />
+          <div className='category-bar'>
+            {categories.map(category => <div key={category}>{category}</div>)}
+          </div>
+        </header>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+
+
+        <footer className='footer'>
+          <div className='btt'>Back to top</div>
+          <div className='contact-link'>Contact links</div>
+          <div className='locale'>Locale selector</div>
+          <div className='others'>Other services</div>
+          <div className='about'>About</div>
+        </footer>
       </div>
-      <footer className='footer'>
-        <div className='btt'>Back to top</div>
-        <div className='contact-link'>Contact links</div>
-        <div className='locale'>Locale selector</div>
-        <div className='others'>Other services</div>
-        <div className='about'>About</div>
-      </footer>
-    </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <div>
+    <div className='bg-image'>
+      <img src={bgImg} style={{ width: '100%' }}></img>
+    </div>
+    <div className='content'>
+      <div>Deals Section</div>
+      <div>Carousal Section</div>
+      <ProductSection />
+    </div>
+  </div>;
+}
+
+function Cart() {
+  return <div>Cart</div>;
 }
 
 function Navbar() {
   return <div className='navbar'>
     <div className='navbar-left'>
-      <div className='logo'>Logo</div>
+      <Link to={'/'}><div className='logo'>Logo</div></Link>
+
       <div className='location'>Location</div>
     </div>
     <div className='navbar-middle'>
@@ -46,7 +64,7 @@ function Navbar() {
       <div className='language'>Language</div>
       <div className='account'>Account</div>
       <div className='orders'>Orders</div>
-      <div className='cart'><span style={{ fontWeight: 'bolder' }}>{3}</span> Cart</div>
+      <Link to={'/cart'}><div className='cart'><span style={{ fontWeight: 'bolder' }}>{3}</span> Cart</div></Link>
     </div>
   </div>;
 }
