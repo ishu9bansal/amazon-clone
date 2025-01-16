e67d74f2ec0c526abb1721e2789fb885e069369e
Ishu Bansal <ishubansal1400@gmail.com>
2025-01-16 08:51:55 +0530

    card and carousal overlap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import bgImg from './assets/carousalImg8.jpg';
+import proImg from './assets/amazonproduct1.jpg'
 
 const categories = ["All", "Fresh", "MX Player", "Sell", "Lists", "Amazon Pay"];
-
 function App() {
   return (
     <div className="App">
@@ -26,10 +27,14 @@ function App() {
         </div>
       </header>
       <div>
-        <div>Carousal</div>
-        <div>Deals Section</div>
-        <div>Carousal Section</div>
-        <div>Products Section</div>
+        <div className='bg-image'>
+          <img src={bgImg} style={{ width: '100%' }}></img>
+        </div>
+        <div className='content'>
+          <div>Deals Section</div>
+          <div>Carousal Section</div>
+          <ProductSection />
+        </div>
       </div>
       <footer className='footer'>
         <div className='btt'>Back to top</div>
@@ -41,5 +46,30 @@ function App() {
     </div>
   );
 }
+
+function ProductSection() {
+  return <div className='product-section'>
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+  </div>;
+}
+
+function ProductCard() {
+  return <div class="card">
+    <img src={proImg} alt="Card Image" class="card-image" />
+    <div class="card-content">
+      <h2 class="card-title">Card Title</h2>
+      <p class="card-description">This is a description of the card. It gives more detail about the content.</p>
+      <button class="card-button">Learn More</button>
+    </div>
+  </div>
+}
+
 
 export default App;
